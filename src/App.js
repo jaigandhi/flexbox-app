a0c32d1753a27d84bdbd5192df52c25494a3af22
Jai gandhi <46311947+jaigandhi@users.noreply.github.com>
2019-09-01 16:16:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+     <div class="wrapper">
+  <header class="header">Header</header>
+  <section class="content">
+    <div class="columns">
+      <main class="main"><center>2</center> </main>
+      <aside class="sidebar-first"><center>1</center></aside>
+      <aside class="sidebar-second"><center>3</center></aside>
+    </div>
+  </section>
+  <footer class="footer">Footer</footer>
+</div>
+      
+    </div>
+  );
+}
+
+export default App;
